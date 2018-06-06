b78e7d0b8b86433ff092ba90b37b834da0662a4a
amarsr13686@gmail.com <amarsr13686@gmail.com>
2018-06-06 00:49:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ function happyHolidays() {
   return "Happyholidays!"
 }
 function happyHolidaysTo(name) {
-  return `Happy
+  return `Happy holidays, ${name}!`
+}
+function happyHolidaysTo(holidays, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(days, holiday) {
+  return `It's ${days} days until ${holiday}!`
 }
